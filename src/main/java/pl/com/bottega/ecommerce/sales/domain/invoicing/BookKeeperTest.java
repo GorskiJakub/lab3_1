d98c71ab38ca3d2e8c4813a941c12f6a9c5a2ea2
GorskiJakub <jakubg93@o2.pl>
2015-04-23 23:46:14 +0200

    Dodanie testu sprawdzającego czy żadanie faktury z dwoma pozycjami wywoła dwukrotnie metodę calculateTax

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,8 +2,12 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 import java.util.Date;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import pl.com.bottega.cqrs.command.handler.*;
 import pl.com.bottega.ecommerce.canonicalmodel.*;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -14,13 +18,15 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
+	public BookKeeper bookKeeper;
+
 	@Test
 	public void testCase1_IfRequestedFactureWithOnePostition_ShouldReturnOnePosition() {
 
 		Money money = new Money(20);
 		Id id = new Id("1");
 
-		ClientData clientData = new ClientData(id, "Faktura");
+		ClientData clientData = new ClientData(id, "klient");
 
 		BookKeeper book;
 
@@ -47,5 +53,34 @@ public class BookKeeperTest {
 
 		assertThat(result, is(1));
 
+	}
+
+	@Test
+	public void testcase_2_IFPositionInvoiceRequested_callCalculateTaxTwoTimes() {
+
+		Id id = new Id("1");
+		Money moneyEveryItem = new Money(1);
+		ProductType productTypeEveryItem = ProductType.FOOD;
+		ClientData clientData = new ClientData(id, "klient");
+		ProductData productData = new ProductData(id, moneyEveryItem,
+				"Faktura", productTypeEveryItem, new Date());
+		RequestItem requestItem = new RequestItem(productData, 4,
+				moneyEveryItem);
+
+		InvoiceFactory mockInvoiceFactory = mock(InvoiceFactory.class);
+		bookKeeper = new BookKeeper(mockInvoiceFactory);
+		when(mockInvoiceFactory.create(clientData)).thenReturn(
+				new Invoice(id, clientData));
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		when(taxPolicy.calculateTax(productTypeEveryItem, moneyEveryItem))
+				.thenReturn(new Tax(moneyEveryItem, "spis"));
+
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+		invoiceRequest.add(requestItem);
+
+		Invoice invoiceResult = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+		Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax(
+				productTypeEveryItem, moneyEveryItem);
 	}
 }
